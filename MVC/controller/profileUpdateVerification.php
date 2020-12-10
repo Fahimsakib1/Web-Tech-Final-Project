64bc1acc6a286448e456506183619ebc10eea7f1
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2020-12-10 16:04:56 +0600

    New CSS features added

--- MVC/controller/profileUpdateVerification.php
+++ MVC/controller/profileUpdateVerification.php
@@ -9,12 +9,19 @@
             $mobileErr = "Mobile number must have 11 digits";
         }
         else{
-            $mobile=$_POST["mobile"];
-            $user = fopen("../asset/data/mobileNumber.txt", "w") or die("Unable to open file!");
-            fwrite($user, $mobile);
-            fwrite($user, "\n");
-            fclose($user);
-            $mobileOk = "Update Successful";
+            $number=$_POST['mobile'];
+            $pattern = "/^[0-9]+$/";
+            if(!preg_match($pattern, $number)){
+                $mobileErr = "Only Numbers are allowed";
+            }
+            else{
+                $mobile=$_POST["mobile"];
+                $user = fopen("../asset/data/mobileNumber.txt", "w") or die("Unable to open file!");
+                fwrite($user, $mobile);
+                fwrite($user, "\n");
+                fclose($user);
+                $mobileOk = "Update Successful";
+            }
         }
     }
 ?>
