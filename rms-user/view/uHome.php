d692255aa7d7b18852c97c6c93ea3996d8da477f
MD Hasibur Rahman Tomal <hr.tomal@gmail.com>
2021-01-02 20:43:21 +0600

    css updated

--- rms-user/view/uHome.php
+++ rms-user/view/uHome.php
@@ -1,45 +1,66 @@
-<?php
-include 'header.php'
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
-
-	<style>
-		.grid-container {
-  		display: inline-grid;
-  		background-color: white;
-  		padding: 10px;
-  		grid-template-columns: auto auto auto auto;
-  		align-content: space-evenly;
-  		grid-gap: 20px 40px;
-
-		}
-	</style>
-
-	<h1 style="text-align: center;">Welcome user!!!</h1>
-
+	<link rel="stylesheet" href="../view/css/homepage.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<script src="../view/js/midChange.js"></script>
 </head>
-<body style="background-color:#cccccc;">
-	<center>
-		<hr>
-		<div class="grid-container">	
-			<input type=button onClick="location.href='http://localhost/rms/view/purchaseTicket.php'" value='Purchase Ticket'>
-			<input type=button onClick="location.href='http://localhost/rms/view/returnTicket.php'" value='Return Ticket'>
-			<input type=button onClick="location.href='http://localhost/rms/view/activeTickets.php'" value='Active Tickets'>
-			<input type=button onClick="location.href='http://localhost/rms/view/purchaseHistory.php'" value='Purchase History'>
-			<input type=button onClick="location.href='http://localhost/rms/view/trainInfo.php'" value='Train info'>
-			<input type=button onClick="location.href='http://localhost/rms/view/searchTrain.php'" value='Search Train'>
-			<input type=button onClick="location.href='http://localhost/rms/view/profile.php'" value='Personal Profile'>
-			<!-- <input type=button onClick="location.href='http://localhost/rms/view/profile.php'" value='Fare'> -->
-			<input type=button onClick="location.href='http://localhost/rms/index.php'" value='Logout'>
-		</div>
-	</center>
+<body>
+<div class="grid-container">
+  <div class="header">  
+    <h2>
+    	<img src="../view/image/rail_logo.png" alt="Railway" width="10%">
+    	Welcome to Railway
+    	<img src="../view/image/rail_logo2.png" alt="Railway" width="10%">
+	</h2>
+  </div>
+  
+  <div class="left" style="background-color:#aaa;">
+  	<center>
+		<button class="w3-btn w3-block w3-teal" onClick="location.href='http://localhost/rms/view/purchaseTicket.php'">Pruchase Ticket</button>
+		<br>
+		<button class="w3-btn w3-block w3-teal"  onclick="myFunction()">Return Ticket</button>
+		<br>
+		<button class="w3-btn w3-block w3-teal">Active Tickets</button>
+		<br>
+		<button class="w3-btn w3-block w3-teal">Purchase History</button>
+		<br>
+		<button class="w3-btn w3-block w3-teal">See Fare</button>
+		<br>
+		<button class="w3-btn w3-block w3-teal">Train Info</button>
+		<br>
+		<button class="w3-btn w3-block w3-teal">Search Train</button>
+		<br>
+		<button class="w3-btn w3-block w3-teal">Personal Profile</button>
+		<br>
+		<button class="w3-button w3-block w3-red">Logout</button>
 
+		
+	</center>
+  </div>
+
+  <div class="middle" id="mid" style="background-color:#bbb;">
+  	<img src="../view/image/train1.jpg" width="100%" height="80%">
+  	<p style="font-weight: bold; font-size: 20px; text-align: center;">
+  		Welcome to Railway E-Ticketing Service.
+  	</p>
+
+  </div>  
+
+  <div class="right" style="background-color:#ccc;">
+  	<h4 style="background-color: red;">Notice</h4>
+  	<div class="notice">
+  		<p>
+  		Welcome to Railway department!!! This notice board is under maintenance.
+  		</p>
+  	</div>  	
+  </div>
+  
+  <div class="footer">
+    <?php include 'footer.php';?>
+  </div>
+</div>
 
 </body>
 </html>
 
-<?php include 'footer.php';?>
